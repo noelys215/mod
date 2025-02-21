e788787731b434fdde683a874d091f9e3233d8ce
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-21 13:00:18 -0500

    chore: write fetchPosts function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 export default function Home() {
+	const url = 'https://jsonplaceholder.typicode.com/posts';
 	// Fetch Posts from API (limit 10)
+
+	const fetchPosts = async () => {
+		try {
+			const res = await fetch(url);
+			const data = await res.json();
+			console.log(data);
+		} catch (error) {
+			console.log('Error fetching posts:' + error);
+		}
+	};
+	fetchPosts();
 
 	return (
 		<div>
