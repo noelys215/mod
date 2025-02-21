e32057236c74c95eb9b60a74393ed5b4e539cab4
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-21 13:18:07 -0500

    chore: rework fetchPosts function away from React methos to Nextjs methods from Nextjs  docs to fix server error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,27 @@
 const fetchPosts = async (page: number) => {
 	// Fetch Posts from API (limit 10)
 	const url = `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`;
-
-	try {
-		const res = await fetch(url);
-		const data = await res.json();
-		console.log(data);
-	} catch (error) {
-		console.log('Error fetching posts:' + error);
-	}
+	const res = await fetch(url);
+	if (!res.ok) console.log('Error fetching posts');
+	return res.json();
 };
 
 export default async function Home({ pageParams }: { pageParams: { page: string } }) {
-	const page = Number(pageParams?.page) || 1;
+	const page = Number(pageParams?.page) || 2;
 	const posts = await fetchPosts(page);
 
 	console.log(posts);
 	return (
 		<div>
-			{/* Display Posts Detail Page */}
-			<h1>Home</h1>
+			<h1>Posts</h1>
+			<ul>
+				{posts.map((post: { id: number; title: string; body: string }) => (
+					<li key={post.id}>
+						<h2>{post.title}</h2>
+						<p>{post.body}</p>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
