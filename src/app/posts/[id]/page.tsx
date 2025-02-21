3636a93cb3ccf8df8d4d3e85967b6a4355565819
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-21 14:36:37 -0500

    change google font

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -14,13 +14,24 @@ export default async function PostDetailPage({ params }: PostDetailProps) {
 	if (!post) return null;
 
 	return (
-		<article aria-label="post-title">
-			<h1 id="post-title">{post?.title}</h1>
-			<p>{post?.body}</p>
+		<div className="max-w-2xl mx-auto py-10 px-5">
+			<article
+				aria-label="post-title"
+				className="border border-gray-200 p-6 rounded-lg shadow-md">
+				<h1 id="post-title" className="text-3xl font-bold text-pink-900">
+					{post?.title}
+				</h1>
+				<p className="mt-4 text-lg text-gray-700">{post?.body}</p>
 
-			<Link href="/" aria-label="Go to Home Page">
-				Return Home
-			</Link>
-		</article>
+				<div className="mt-8 text-center">
+					<Link
+						href="/"
+						aria-label="Go to Home Page"
+						className="inline-block px-6 py-3 bg-gray-600 text-white rounded-lg hover:bg-blue-700 transition">
+						Return Home
+					</Link>
+				</div>
+			</article>
+		</div>
 	);
 }
