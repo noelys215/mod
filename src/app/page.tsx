d80912533fff8303c0ef02f0769099dd6843d4d4
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2025-02-21 13:12:51 -0500

    chore: work on pagination and page limits (query params)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
-export default function Home() {
-	const url = 'https://jsonplaceholder.typicode.com/posts';
+const fetchPosts = async (page: number) => {
 	// Fetch Posts from API (limit 10)
+	const url = `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`;
 
-	const fetchPosts = async () => {
-		try {
-			const res = await fetch(url);
-			const data = await res.json();
-			console.log(data);
-		} catch (error) {
-			console.log('Error fetching posts:' + error);
-		}
-	};
-	fetchPosts();
+	try {
+		const res = await fetch(url);
+		const data = await res.json();
+		console.log(data);
+	} catch (error) {
+		console.log('Error fetching posts:' + error);
+	}
+};
 
+export default async function Home({ pageParams }: { pageParams: { page: string } }) {
+	const page = Number(pageParams?.page) || 1;
+	const posts = await fetchPosts(page);
+
+	console.log(posts);
 	return (
 		<div>
 			{/* Display Posts Detail Page */}
